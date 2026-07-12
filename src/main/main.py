34ec4920b31018d85fe5363d3c5a8140bec32f7f
rosemya <rosemya@github.com>
2026-07-12 19:19:23 +0000

    Test run: 3/4 passed, 1 failed, 0 skipped - 2026-07-12T19:19:23.213Z

--- src/main/main.py
+++ src/main/main.py
@@ -104,11 +104,11 @@ def write_ordered_calls(csv_file_path):
     with open(csv_file_path, "w") as csv_file:
         csv_file.write("phoneNumber, startTime, endTime, direction, userId\n")
         csv_file.write("1, 11, 5, inbound, 1, 1\n")
-        csv_file.write("3, 21, 5, inbound, 7\n")
-        csv_file.write("5, 31, 5, inbound, 3\n")
-        csv_file.write("7, 41, 5, inbound, 5\n")
-        csv_file.write("2, 51, 5, outbound, 2\n")
-        csv_file.write("4, 71, 5, outbound, 4\n")
+        csv_file.write("3, 21, 5, inbound, 1, 7\n")
+        csv_file.write("5, 31, 5, inbound, 1, 3\n")
+        csv_file.write("7, 41, 5, inbound, 1, 5\n")
+        csv_file.write("2, 51, 5, outbound, 1, 2\n")
+        csv_file.write("4, 71, 5, outbound, 1, 4\n")
 
 
 # No need to touch the functions below!------------------------------------------
