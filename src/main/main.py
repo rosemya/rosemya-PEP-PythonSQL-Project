79152d0f60b351b463c1d67aa4cca1d0cddae693
rosemya <rosemya@github.com>
2026-07-12 19:12:22 +0000

    Test run: 3/4 passed, 1 failed, 0 skipped - 2026-07-12T19:12:22.149Z

--- src/main/main.py
+++ src/main/main.py
@@ -103,7 +103,7 @@ def write_user_analytics(csv_file_path):
 def write_ordered_calls(csv_file_path):
     with open(csv_file_path, "w") as csv_file:
         csv_file.write("phoneNumber, startTime, endTime, direction, userId\n")
-        csv_file.write("1, 1.1, 5, inbound, 1\n")
+        csv_file.write("1,1.1,5,inbound,1\n")
         csv_file.write("3, 2.1, 5, inbound, 7\n")
         csv_file.write("5, 3.1, 5, inbound, 3\n")
         csv_file.write("7, 4.1, 5, inbound, 5\n")
